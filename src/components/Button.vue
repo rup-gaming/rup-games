97afa319f3001d28f68446087acee70fd8052ce8
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-07 18:57:44 -0700

    feat: add button component, init game

---
+++ src/components/Button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+// import { ref } from "vue";
+const props = defineProps(["text"]);
+</script>
---
+++ src/game/game.ts
@@ -0,0 +1,8 @@
+import { Engine } from "excalibur";
+
+const game = new Engine({
+  width: 800,
+  height: 600,
+});
+
+game.start();
