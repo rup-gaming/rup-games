d77d793dd6aa47fdf654d5217442a57166a07cb3
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-08 18:40:31 -0700

    feat: initialize app layout, import fonts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,49 @@
 <template>
-    <h1>Square Game</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/play">Play</router-link>
-    <router-view></router-view>
+  <n-config-provider :theme="darkTheme">
+    <n-layout style="height: 100vh" :native-scrollbar="false">
+      <n-layout-header bordered>
+        <router-link color="red" to="/">Home</router-link>
+        <router-link to="/play">Play</router-link>
+      </n-layout-header>
+      <n-layout
+        :native-scrollbar="false"
+        content-style="padding: 24px;"
+        class="main-layout"
+      >
+        <router-view></router-view>
+      </n-layout>
+      <n-layout-footer bordered> Footer </n-layout-footer>
+    </n-layout>
+  </n-config-provider>
 </template>
 
 <script setup lang="ts">
+import { defineComponent } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import {
+  darkTheme,
+  NConfigProvider,
+  NLayout,
+  NLayoutHeader,
+  NLayoutFooter,
+} from "naive-ui";
+
+defineComponent({
+  darkTheme,
+});
 </script>
+
+<style scoped>
+.main-layout {
+  height: 100vh;
+  display: flex;
+}
+
+.n-layout-header {
+  padding: 24px;
+}
+
+.n-layout-footer {
+  padding: 24px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./navigation/router";
 
+// General Font
+import "vfonts/Lato.css";
+// Monospace Font
+import "vfonts/FiraCode.css";
+
 const vueApp = createApp(App);
 
 vueApp.use(router);
