c69ecb54ca1fc8a0dc7629237c5582505babb52f
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-10 15:25:59 -0700

    feat: create header drawer for mobile app header

--- src/components/RupHeader.vue
+++ src/components/RupHeader.vue
@@ -1,12 +1,14 @@
 <template>
   <n-layout-header bordered>
     <div class="side-container-left">
-      <n-button text :focusable="false">
-        <n-icon size="30" class="logo">
-          <diamond-sharp />
-        </n-icon>
-        <n-h3 class="title">Rup Games</n-h3>
-      </n-button>
+      <router-link to="/" style="text-decoration: none; height: 30px">
+        <n-button text :focusable="false">
+          <n-icon size="30" class="logo">
+            <diamond-sharp />
+          </n-icon>
+          <n-h3 class="title">Rup Games</n-h3>
+        </n-button>
+      </router-link>
     </div>
 
     <div
@@ -65,22 +67,72 @@
         Light
       </n-button>
 
-      <n-button text style="margin-left: 20px">
-        <n-icon
-          size="30"
-          v-show="store.windowWidth === 'xs' || store.windowWidth === 'sm'"
-        >
+      <n-button
+        text
+        :focusable="false"
+        @click="activateDrawer"
+        v-show="store.windowWidth === 'xs' || store.windowWidth === 'sm'"
+      >
+        <n-icon size="30">
           <menu-sharp />
         </n-icon>
       </n-button>
     </div>
+    <n-drawer v-model:show="showDrawer" :width="240" placement="right">
+      <n-drawer-content>
+        <n-menu
+          :value="`${currentRoute.name}`"
+          mode="vertical"
+          :options="appNavigation"
+        />
+        <div class="drawer-theme-btn-container">
+          <n-button
+            ghost
+            class="drawer-theme-btn"
+            @click="disableDarkMode(true)"
+            type="default"
+            v-if="!store.darkMode"
+          >
+            <template #icon>
+              <n-icon size="20">
+                <dark-mode-outlined />
+              </n-icon>
+            </template>
+            Dark
+          </n-button>
+
+          <n-button
+            ghost
+            class="drawer-theme-btn"
+            @click="disableDarkMode(false)"
+            type="default"
+            v-else
+          >
+            <template #icon>
+              <n-icon size="20">
+                <light-mode-outlined />
+              </n-icon>
+            </template>
+            Light
+          </n-button>
+        </div>
+      </n-drawer-content>
+    </n-drawer>
   </n-layout-header>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, h } from "vue";
+import { defineComponent, h, ref } from "vue";
 import { RouterLink, useRoute } from "vue-router";
-import { NLayoutHeader, NIcon, NH3, NMenu, NButton } from "naive-ui";
+import {
+  NLayoutHeader,
+  NIcon,
+  NH3,
+  NMenu,
+  NButton,
+  NDrawer,
+  NDrawerContent,
+} from "naive-ui";
 import type { MenuOption } from "naive-ui";
 import { useStore } from "../store/store";
 import {
@@ -93,8 +145,14 @@ import {
 const store = useStore();
 const currentRoute = useRoute();
 
+const showDrawer = ref(false);
+
 function disableDarkMode(isDark: boolean) {
   store.setDarkMode(isDark);
+}
+
+function activateDrawer() {
+  showDrawer.value = !showDrawer.value;
 }
 
 const appNavigation: MenuOption[] = [
@@ -155,7 +213,6 @@ defineComponent({
 .side-container-right {
   display: flex;
   align-items: center;
-  justify-content: flex-end;
 }
 
 .side-container-item {
@@ -180,5 +237,17 @@ defineComponent({
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.drawer-theme-btn-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+
+.drawer-theme-btn {
+  width: 176px;
+  height: 42px;
 }
 </style>
