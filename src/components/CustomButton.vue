879b5d89feb9ebc45a0521c7dc408781b867c266
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-04-11 11:10:25 -0700

    feat: install supabase, configure supabase

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,15 +1,15 @@
 <template>
-    <button @click="props.handleClick()">{{ text }}</button>
+  <button @click="props.handleClick()">{{ text }}</button>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-    text: String,
-    handleClick: {
-        type: Function,
-        default() {
-            return console.log("Clicked!");
-        },
+  text: String,
+  handleClick: {
+    type: Function,
+    default() {
+      return console.log("Clicked!");
     },
+  },
 });
 </script>
